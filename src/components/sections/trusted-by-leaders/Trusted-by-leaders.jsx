bc16453a39c06396c8888c3fab4f2d6f39abc1b4
Ruslan Nikolaiets <mnemonikus69@gmail.com>
2024-01-28 20:14:18 +0200

    finished refactoring trust-by-leaders section

--- src/components/sections/trusted-by-leaders/Trusted-by-leaders.jsx
+++ src/components/sections/trusted-by-leaders/Trusted-by-leaders.jsx
@@ -1,14 +1,5 @@
 import styles from './trusted-by-leaders.module.scss';
 
-// import icon1 from '../../../img/sections/trusted-by-leaders/raleigh.webp';
-// import icon2 from '../../../img/sections/trusted-by-leaders/fdot.webp';
-// import icon3 from '../../../img/sections/trusted-by-leaders/us-davis.webp';
-// import icon4 from '../../../img/sections/trusted-by-leaders/city-of-aspen.webp';
-// import icon5 from '../../../img/sections/trusted-by-leaders/walker.webp';
-// import icon6 from '../../../img/sections/trusted-by-leaders/california.webp';
-// import icon7 from '../../../img/sections/trusted-by-leaders/premium-parking.webp';
-// import icon8 from '../../../img/sections/trusted-by-leaders/signature.webp';
-
 import { staticData } from '../../../static-data/static-data';
 
 import { AnimationOnScroll } from 'react-animation-on-scroll';
@@ -25,60 +16,21 @@ const TrustedByLeaders = ({ hrefId }) => {
 
 				<p className={styles.trusted_by__bottom_text}>Here's who we've worked with</p>
 
-				<div className={styles.trusted_by__logos_wrapper}>
+				<ul className={styles.trusted_by__logo_list}>
 					{staticData.trustByData.map((el, index) => {
+						const delay = (index % 4) * 100;
+
 						return (
-							<svg className={styles.trusted_by__logo_item}>
-								<use xlinkHref={el.path} />
-							</svg>
+							<li key={index} className={styles.trusted_by__logo_list__item}>
+								<AnimationOnScroll delay={delay} animateIn="animate__wobble">
+									<svg className={styles.trusted_by__logo__icon}>
+										<use xlinkHref={el.path} />
+									</svg>
+								</AnimationOnScroll>
+							</li>
 						);
 					})}
-
-					{/* <div className={styles.iconBlockWrapper}> */}
-					{/* <div className={styles.iconWrapper}>
-							<AnimationOnScroll animateIn="animate__wobble">
-								<img src={icon1} width="100%" height="100%" alt="icon" />
-							</AnimationOnScroll>
-						</div>
-						<div className={styles.iconWrapper}>
-							<AnimationOnScroll delay={100} animateIn="animate__wobble">
-								<img src={icon2} width="100%" height="100%" alt="icon" />
-							</AnimationOnScroll>
-						</div>
-						<div className={styles.iconWrapper}>
-							<AnimationOnScroll delay={200} animateIn="animate__wobble">
-								<img src={icon3} width="100%" height="100%" alt="icon" />
-							</AnimationOnScroll>
-						</div>
-						<div className={styles.iconWrapper}>
-							<AnimationOnScroll delay={300} animateIn="animate__wobble">
-								<img src={icon4} width="100%" height="100%" alt="icon" />
-							</AnimationOnScroll>
-						</div> */}
-					{/* </div> */}
-					{/* <div className={styles.iconBlockWrapper}> */}
-					{/* <div className={styles.iconWrapper}>
-							<AnimationOnScroll animateIn="animate__wobble">
-								<img src={icon5} width="100%" height="100%" alt="icon" />
-							</AnimationOnScroll>
-						</div>
-						<div className={styles.iconWrapper}>
-							<AnimationOnScroll delay={100} animateIn="animate__wobble">
-								<img src={icon6} width="100%" height="100%" alt="icon" />
-							</AnimationOnScroll>
-						</div>
-						<div className={styles.iconWrapper}>
-							<AnimationOnScroll delay={200} animateIn="animate__wobble">
-								<img src={icon7} width="100%" height="100%" alt="icon" />
-							</AnimationOnScroll>
-						</div>
-						<div className={styles.iconWrapper}>
-							<AnimationOnScroll delay={300} animateIn="animate__wobble">
-								<img src={icon8} width="100%" height="100%" alt="icon" />
-							</AnimationOnScroll>
-						</div> */}
-					{/* </div> */}
-				</div>
+				</ul>
 			</section>
 		</div>
 	);
